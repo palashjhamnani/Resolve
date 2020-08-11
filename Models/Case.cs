94475406976357712d5ca2279e3a43f1a8c09cd7
anyalynn <anyal@uw.edu>
2020-08-11 15:24:59 -0700

    Adding lastest case types

--- Models/Case.cs
+++ Models/Case.cs
@@ -47,6 +47,8 @@ namespace Resolve.Models
         public ICollection<HRServiceStaff> HRServiceStaff { get; set; }
         public ICollection<HRServiceFaculty> HRServiceFaculty { get; set; }
         public ICollection<PerioLimitedCare> PerioLimitedCare { get; set; }
+        public ICollection<HiringAffiliateFaculty> HiringAffiliateFaculty { get; set; }
+        public ICollection<HRServiceScholarResident> HRServiceScholarResident { get; set; }
 
     }  
 
